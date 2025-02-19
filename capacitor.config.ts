21189bb8a464a61c104234c4822a233624a52ce3
Nassim Allagui <allagui45@gmail.com>
2025-02-19 14:42:57 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'receipt-app-blank',
+  webDir: 'www'
+};
+
+export default config;
